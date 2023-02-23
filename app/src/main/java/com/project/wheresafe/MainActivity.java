83f03e407d3eb54771196d9c638decaf7fbd2bcc
Suraj-09 <33708841+Suraj-09@users.noreply.github.com>
2023-02-23 18:44:31 -0500

    fixed missing resources

--- app/src/main/java/com/project/wheresafe/MainActivity.java
+++ app/src/main/java/com/project/wheresafe/MainActivity.java
@@ -1,23 +1,17 @@
 package com.project.wheresafe;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.project.wheresafe.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/project/wheresafe/ui/team/TeamFragment.java
@@ -0,0 +1,38 @@
+package com.project.wheresafe.ui.team;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.project.wheresafe.databinding.FragmentTeamBinding;
+
+public class TeamFragment extends Fragment {
+
+    private FragmentTeamBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        TeamViewModel teamViewModel =
+                new ViewModelProvider(this).get(TeamViewModel.class);
+
+        binding = FragmentTeamBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textTeam;
+        teamViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

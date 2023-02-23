1e5c10be5b1dd590ca377180d1eafcfdc4dd5422
Alexander Kepekci <alexkepekci@hotmail.com>
2023-02-23 18:12:23 -0500

    Bottom + side navigation WhereSafe Logo on launch and menu

--- app/src/main/java/com/project/wheresafe/ui/home/HomeViewModel.java
+++ app/src/main/java/com/project/wheresafe/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Home :)");
     }
 
     public LiveData<String> getText() {
